02884a9f454b8534789f96c6da7caef4ff60baa6
pierre schuvey <chromatykretro@gmail.com>
2023-02-26 19:32:50 +0100

    oui

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="test" element={<CreatePost />} />
+        <Route path="/test" element={<CreatePost />} />
         <Route path="/" element={<Pokedex />} />
-        <Route path="sa54LEn76V4Lng" element={<FirstGen />} />
-        <Route path="DPbwt8A27bi26V" element={<SecondGen />} />
-        <Route path="u7fNSv6GfBg328" element={<ThirdGen />} />
-        <Route path="E638Q8Xyvf3Tfy" element={<FourthGen />} />
-        <Route path="uT6V5J8b7eHx4n" element={<FifthGen />} />
-        <Route path="RC5dm9zwcW2G55" element={<SixthGen />} />
-        <Route path="d76C6Xx9MaVnc9" element={<SeventhGen />} />
-        <Route path="6NFptvb6V74w5J" element={<EightGen />} />
-        <Route path="y5d3w62KQiLyP3" element={<NinthGen />} />
-        <Route path="26tQ3JasgSK44y" element={<AllGen />} />
-        <Route path="MY89s2Dv54zsuF" element={<Legendary />} />
+        <Route path="/sa54LEn76V4Lng" element={<FirstGen />} />
+        <Route path="/DPbwt8A27bi26V" element={<SecondGen />} />
+        <Route path="/u7fNSv6GfBg328" element={<ThirdGen />} />
+        <Route path="/E638Q8Xyvf3Tfy" element={<FourthGen />} />
+        <Route path="/uT6V5J8b7eHx4n" element={<FifthGen />} />
+        <Route path="/RC5dm9zwcW2G55" element={<SixthGen />} />
+        <Route path="/d76C6Xx9MaVnc9" element={<SeventhGen />} />
+        <Route path="/6NFptvb6V74w5J" element={<EightGen />} />
+        <Route path="/y5d3w62KQiLyP3" element={<NinthGen />} />
+        <Route path="/26tQ3JasgSK44y" element={<AllGen />} />
+        <Route path="/MY89s2Dv54zsuF" element={<Legendary />} />
 
       </Routes>
     </BrowserRouter>
