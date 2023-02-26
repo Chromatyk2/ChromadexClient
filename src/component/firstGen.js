bc990d971189e02217ec39535c76c11488f031a6
pierre schuvey <chromatykretro@gmail.com>
2023-02-26 21:47:59 +0100

    oui

--- src/component/firstGen.js
+++ src/component/firstGen.js
@@ -52,7 +52,7 @@ function UniqueFirstGenPokemon(props) {
    if(name[4] !== undefined && items.sprites !== undefined){
      const shinyOdd = Math.floor((Math.random() * 1365) + 1);
      if(shinyOdd == 1){
-         Axios.post('https://chromatyk-pokemon.herokuapp.com/api/capture', {pseudo: pseudo, pkmName: name[4].name, pkmImage:items.sprites.other.home.front_shiny, shiny:1})
+         Axios.post('https://chromatyk-pokemon.herokuapp.com/api/capture', {pseudo: pseudo, pkmName: name[4].name, pkmImage:items.sprites.other.home.front_shiny,pkmId:items.id, shiny:1})
        return (
          <>
          <p className="pkmName">{name[4].name}<img className="shinySpark" src="https://res.cloudinary.com/shiny24/image/upload/v1669396824/pokemon/shiny_symbol_pokemon_tdxjdc.png"></img></p>
@@ -62,7 +62,7 @@ function UniqueFirstGenPokemon(props) {
         </>
        );
      }else{
-         Axios.post('https://chromatyk-pokemon.herokuapp.com/api/capture', {pseudo: pseudo, pkmName: name[4].name, pkmImage:items.sprites.other.home.front_shiny,pkmId:items.id shiny:0})
+         Axios.post('https://chromatyk-pokemon.herokuapp.com/api/capture', {pseudo: pseudo, pkmName: name[4].name, pkmImage:items.sprites.other.home.front_shiny,pkmId:items.id, shiny:0})
        return (
          <>
          <p className="pkmName">{name[4].name}</p>
