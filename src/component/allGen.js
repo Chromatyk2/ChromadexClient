62ba3fc6873e35f25c10d6b65006abcf62f4c3a5
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 20:53:23 +0100

    legendary

--- src/component/allGen.js
+++ src/component/allGen.js
@@ -18,12 +18,11 @@ function UniqueFirstGenPokemon(props) {
   const [isLegendary, setIsLegendary] = useState(false);
   const legendaryChance = Math.floor((Math.random() * 1) + 1);
   const randomLegendary = [144,145,146,150,151,243,244,245,249,250,251,377,378,379,380,381,382,383,384,385,386,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,638,639,640,641,642,643,644,645,646,647,648,649,716,717,718,719,720,721,785,786,787,788,789,790,791,792,793,794,795,796,797,798,799,800,801,802,803,804,805,806,807,888,889,890,891,892,893,894,895,896,897,898,905]
-  // var randomNumber = Math.floor((Math.random() * 905) + 1);
-  var randomNumber = 151;
+  var randomNumber = Math.floor((Math.random() * 905) + 1);
   if(randomLegendary.includes(randomNumber) === true && legendaryChance != 100){
       // var randomNumber = randomLegendary[Math.floor(Math.random()*randomLegendary.length)];
       while (randomLegendary.includes(randomNumber) === true && legendaryChance != 100) {
-        randomNumber = 151;
+        randomNumber = Math.floor((Math.random() * 905) + 1);
       }
   }else{
       randomNumber = 151;
