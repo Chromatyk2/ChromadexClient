3b217ac52f1f90af5fe9876226893aa6157a2551
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 21:09:12 +0100

    legendary

--- src/component/allGen.js
+++ src/component/allGen.js
@@ -21,10 +21,8 @@ function UniqueFirstGenPokemon(props) {
   // var randomNumber = Math.floor((Math.random() * 905) + 1);
   var randomNumber = 151;
   if(randomLegendary.includes(randomNumber) === true && legendaryChance != 100){
-      console.log("C'est pas un leg batard");
       while (randomLegendary.includes(randomNumber) === false) {
-        // randomNumber = Math.floor((Math.random() * 905) + 1);
-        randomNumber = 151;
+        randomNumber = Math.floor((Math.random() * 905) + 1);
       }
   }else if (randomLegendary.includes(randomNumber) === true && legendaryChance == 100) {
       console.log("C'est un leg GG");
