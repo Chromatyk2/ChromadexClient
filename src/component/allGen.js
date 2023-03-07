3429606f3b73b7c4976d92bbfb6f68b5b096af30
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 21:10:53 +0100

    legendary

--- src/component/allGen.js
+++ src/component/allGen.js
@@ -19,17 +19,16 @@ function UniqueFirstGenPokemon(props) {
   const legendaryChance = Math.floor((Math.random() * 1) + 1);
   const randomLegendary = [144,145,146,150,151,243,244,245,249,250,251,377,378,379,380,381,382,383,384,385,386,480,481,482,483,484,485,486,487,488,489,490,491,492,493,494,638,639,640,641,642,643,644,645,646,647,648,649,716,717,718,719,720,721,785,786,787,788,789,790,791,792,793,794,795,796,797,798,799,800,801,802,803,804,805,806,807,888,889,890,891,892,893,894,895,896,897,898,905]
   // var randomNumber = Math.floor((Math.random() * 905) + 1);
-  var randomNumber = 151;
+  var randomNumber = 1;
+  console.log(randomLegendary.includes(randomNumber));
   if(randomLegendary.includes(randomNumber) === true && legendaryChance != 100){
       while (randomLegendary.includes(randomNumber) === false) {
         randomNumber = Math.floor((Math.random() * 905) + 1);
       }
   }else if (randomLegendary.includes(randomNumber) === true && legendaryChance == 100) {
-      console.log("C'est un leg GG");
     randomNumber = randomLegendary[Math.floor(Math.random()*randomLegendary.length)];
   }else{
     while (randomLegendary.includes(randomNumber) === false) {
-      console.log("Cheh");
       randomNumber = Math.floor((Math.random() * 905) + 1);
     }
   }
