3a53616250b04b0c956b8c5392986c418a6251f7
pierre schuvey <chromatykretro@gmail.com>
2023-03-07 19:59:10 +0100

    legendary

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import { BrowserRouter, Route, Routes} from "react-router-dom";
 import $ from "jquery";
 import './App.css';
 import AllGen from './component/allGen.js';
-import Legendary from './component/legendary.js';
+import Home from './component/home.js';
+
 
 function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route path="/test" element={<CreatePost />} />
-        <Route path="/" element={<Pokedex />} />
+        <Route path="/" element={<Home />} />
         <Route path="/sa54LEn76V4Lng" element={<AllGen />} />
-        <Route path="/MY89s2Dv54zsuF" element={<Legendary />} />
 
       </Routes>
     </BrowserRouter>
---
+++ src/component/home.js
@@ -0,0 +1,7 @@
+import React, {useState, useEffect, Component, useContext, useMemo} from "react";
+
+function UniqueFirstGenPokemon(props) {
+
+export default function Home() {
+  return <p>Bonjour !</p>;
+}
